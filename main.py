4ed84fb47dc0983aefeb1554ae0943eb9b2b671e
Moon-ju-young <amymoon07@khu.ac.kr>
2024-11-10 22:55:37 +0900

    우주선 추가

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 import pygame
-from objects import BlackHole
+from math import sin, cos, radians
+from objects import BlackHole, SpaceShip
 
 WIDTH = 800
 HEIGHT = 800
-FPS = 30
+
+SPACESHIP_ACCLERATION = 0.00002
 
 DARK_BLUE = (50, 50, 100)
 
 GameQuit = False
+clock = pygame.time.Clock()
 
-pygame.init()                                           #초기화
-pygame.display.set_caption("Avoid Blackhole")           #제목
+pygame.init()                                       #초기화
+pygame.display.set_caption("Avoid Blackhole")       #제목
 screen = pygame.display.set_mode((WIDTH,HEIGHT))    #화면 크기
 
-while not GameQuit:
+player = SpaceShip((500,500),screen)
 
-    screen.fill(DARK_BLUE)
+while not GameQuit:
+    clock.tick()
 
     for event in pygame.event.get(): 
-            if event.type == pygame.QUIT: 
-                GameQuit = True 
+        if event.type == pygame.QUIT: 
+            GameQuit = True 
+
+    milliseconds = clock.get_time()
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        player.angle += milliseconds * 0.1
+    if keys[pygame.K_RIGHT]:
+        player.angle -= milliseconds * 0.1
+    player.angle %= 360
+    if keys[pygame.K_UP]:
+        player.velx -= sin(radians(player.angle)) * milliseconds * SPACESHIP_ACCLERATION
+        player.vely -= cos(radians(player.angle)) * milliseconds * SPACESHIP_ACCLERATION
+    elif keys[pygame.K_DOWN]:
+        player.velx += sin(radians(player.angle)) * milliseconds * SPACESHIP_ACCLERATION
+        player.vely += cos(radians(player.angle)) * milliseconds * SPACESHIP_ACCLERATION
+
+    screen.fill(DARK_BLUE)
+    player.update()
+    player.show()
     
     pygame.display.update()
--- objects.py
+++ objects.py
@@ -1,23 +1,51 @@
 import pygame
-from math import cos, sin
+from math import dist
 
 GRAVITY = 50
-BlackBGImage = pygame.image.load("dark.png")
+BlackBGImage = pygame.image.load("black_bg.png")
+SpaceShipImage = pygame.image.load("spaceship.png")             #기본크기 80x80
+SpaceShipImage = pygame.transform.scale(SpaceShipImage, (40,40))#크기 조정
 
 class BlackHole:
-    def __init__(self, xy, r:int, s:pygame.Surface):
+    def __init__(self, xy, r:int, s:pygame.Surface) -> None:
         '''sequence 형태로 xy좌표 위치와 반지름, Surface 입력'''
         self.rad = r//10
         self.ran = r
-        self.x = xy[0]
-        self.y = xy[1]
+        self.xy = (xy[0], xy[1])
         self.screen = s
 
-    def show(self):
+    def show(self) -> None:
+        BlackBGImage.set_alpha(120)
         self.screen.blit(
             pygame.transform.scale(BlackBGImage, (2*self.ran,2*self.ran)),
             (self.x-self.ran, self.y-self.ran))
-        pygame.draw.circle(self.screen, (0,0,0), (self.x,self.y), self.rad)
+        pygame.draw.circle(self.screen, (0,0,0), self.xy, self.rad)
     
     def gravity_acc(self, xy) -> tuple:
-        return ()
+        d = dist(xy, self.xy)
+        if d < self.ran:
+            g = GRAVITY * (self.ran - d) / self.ran
+            xg = g * (self.xy[0] - xy[0]) / d
+            yg = g * (self.xy[1] - xy[1]) / d
+            return (xg, yg)
+        else:
+            return (0,0)
+
+class SpaceShip:
+    def __init__(self, xy, s:pygame.Surface) -> None:
+        '''sequence 형태로 xy좌표 위치와 Surface 입력'''
+        self.angle = 45                 #각은 60분법을 사용
+        self.x = xy[0]
+        self.y = xy[1]
+        self.velx = 0
+        self.vely = 0
+        self.screen = s
+    
+    def show(self) -> None:
+        RotateSpaceShip = pygame.transform.rotate(SpaceShipImage, self.angle)
+        new_rect = RotateSpaceShip.get_rect(center=(self.x,self.y))
+        self.screen.blit(RotateSpaceShip,new_rect)
+
+    def update(self) -> None:
+        self.x += self.velx
+        self.y += self.vely
