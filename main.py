fc34a25c86a0213a259414633975ba181d34fc78
Moon-ju-young <amymoon07@khu.ac.kr>
2024-11-14 00:36:18 +0900

    화면 전환

--- main.py
+++ main.py
@@ -18,25 +18,40 @@ player = SpaceShip(s.WIDTH-50,s.HEIGHT-50,screen)
 
 while not GameQuit:
     clock.tick(s.FPS)
+    milliseconds = clock.get_time()
 
     for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: 
+        if event.type == pygame.QUIT:           #종료 시
             GameQuit = True 
 
-    milliseconds = clock.get_time()
-    keys = pygame.key.get_pressed()
+        elif event.type == pygame.KEYDOWN:      #key 단일 입력
+            if stage.PresentStage == s.STAGEMAIN:
+                if event.key == pygame.K_SPACE:
+                    stage.PresentStage = s.STAGELIST
 
-    if keys[pygame.K_LEFT]:
-        player.angle += milliseconds * 0.1
-    if keys[pygame.K_RIGHT]:
-        player.angle -= milliseconds * 0.1
-    player.angle %= 360
-    if keys[pygame.K_UP]:
-        player.velx -= sin(radians(player.angle)) * s.SPACESHIP_ACCLERATION * milliseconds
-        player.vely -= cos(radians(player.angle)) * s.SPACESHIP_ACCLERATION * milliseconds
-    elif keys[pygame.K_DOWN]:
-        player.velx += sin(radians(player.angle)) * s.SPACESHIP_ACCLERATION * milliseconds
-        player.vely += cos(radians(player.angle)) * s.SPACESHIP_ACCLERATION * milliseconds
+            elif stage.PresentStage == s.STAGELIST:
+                point = 1   #커서?가 가리키는 스테이지
+                if event.key == pygame.K_z:
+                    stage.PresentStage = s.STAGEMAIN
+                elif event.key == pygame.K_SPACE:
+                    stage.PresentStage = point
+
+
+    if stage.PresentStage > 0:
+        keys = pygame.key.get_pressed()         #key 지속 입력
+
+        if keys[pygame.K_LEFT]:
+            player.angle += milliseconds * 0.1
+        if keys[pygame.K_RIGHT]:
+            player.angle -= milliseconds * 0.1
+            player.angle %= 360
+        if keys[pygame.K_UP]:
+            player.velx -= sin(radians(player.angle)) * s.SPACESHIP_ACCLERATION * milliseconds
+            player.vely -= cos(radians(player.angle)) * s.SPACESHIP_ACCLERATION * milliseconds
+        elif keys[pygame.K_DOWN]:
+            player.velx += sin(radians(player.angle)) * s.SPACESHIP_ACCLERATION * milliseconds
+            player.vely += cos(radians(player.angle)) * s.SPACESHIP_ACCLERATION * milliseconds
+    
 
     screen.fill(DARK_BLUE)
     player.update()
--- stages.py
+++ stages.py
@@ -2,22 +2,27 @@ import pygame
 from objects import BlackHole
 import setting as s
 
+
 class Stage:
     def __init__(self, sc) -> None:
         self.screen = sc
         self.PresentStage = s.STAGEMAIN
-        self.stage = [0]
-        self.stage.append( [BlackHole(s.WIDTH/2,s.HEIGHT/2,100,self.screen)] )
+        self.StageList = [0]
+        self.StageList.append( [BlackHole(s.WIDTH/2,s.HEIGHT/2,100,self.screen)] )
+
 
     def show(self) -> None:
-        if self.PresentStage > 0:
-            for i in self.stage[self.PresentStage]:
+        if self.PresentStage > 0:                       #스테이지
+            for i in self.StageList[self.PresentStage]:
                 i.show()
-        elif self.PresentStage == s.STAGEMAIN:
+
+        elif self.PresentStage == s.STAGEMAIN:          #메인
             title = pygame.font.SysFont("Bauhaus 93", 80).render("Space Voyage",True,(255,255,255))
             new_rect = title.get_rect(center=(s.WIDTH/2,s.HEIGHT/4))
             self.screen.blit(title,new_rect)
-        elif self.PresentStage == s.STAGELIST:
+
+        elif self.PresentStage == s.STAGELIST:          #스테이지 리스트
             pass
-        elif self.PresentStage == s.STAGEALLCLEAR:
+
+        elif self.PresentStage == s.STAGEALLCLEAR:      #올클리어
             pass
